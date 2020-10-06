735b57c8fb5f1b20c30d2b0d1104e95ac3e099ad
Tomohiro Komagata <tomohiro.komagata@gmail.com>
2020-10-07 01:03:12 +0900

    test

--- src/vis_client_example.py
+++ src/vis_client_example.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 VIS_URL = "wss://wwwivi:8088/"
 
 # Go to https://webhook.site/#/ copy and paste server link to HTTP_REQUEST_RECEIVER_URL
-HTTP_REQUEST_RECEIVER_URL = "https://webhook.site/38354762-044a-4fa2-917f-8eb5c03cd7eb"
+HTTP_REQUEST_RECEIVER_URL = "https://webhook.site/a0b0bb2a-2128-402c-bdd5-e5fc26854ea5"
 
 DATA_SENDING_DELAY = 2
 WAIT_TIMEOUT = 5
